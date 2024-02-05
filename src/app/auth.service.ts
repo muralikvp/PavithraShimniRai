c3d284d53bfea15e30c3aee765cb67a9c62b4ca9
Murali Krishnan <murali@hopetutors.com>
2024-02-05 11:18:02 +0530

    Commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // Import the HttpClient class
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiurl: string = 'https://dummyjson.com/auth/login';
+  constructor(private http: HttpClient) {} // Use the imported HttpClient class
+
+  CheckLogin(userCredentials: any) {
+    let result = this.http.post(this.apiurl, userCredentials);
+    return result;
+  }
+  IsLoggedIn() {
+    return localStorage.getItem('token') != null;
+  }
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'HopeTutors-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  private firstNumber!: number;
+  private secondNumber: number = 100;
+  public resultArr: string[] = ['Shimmni Rai', 'Pavithra', 'Hope Tutors'];
+  private responseData: any;
+
+  constructor(private auth:AuthService) { }
+
+  Login = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  public ProceedLogin(): void {
+    if (this.Login.valid) {
+
+      this.auth.CheckLogin(this.Login.value).subscribe((response: any) => {
+        console.log(response);
+      });
+      console.log("Inside Login function");
+
+    }
+    else {
+      console.log('Login Failed');
+    }
+
+  }
+
+}
